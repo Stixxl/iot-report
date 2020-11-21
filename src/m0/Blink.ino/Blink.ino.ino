f34b29dc15df32971cc7478a6e0ab2e819df4a25
Corbinian Stiglmair <corbinho@hotmail.de>
2020-11-21 17:37:58 +0100

    fix blink.ino and update paper to current progress

--- src/m0/Blink.ino/Blink.ino.ino
+++ src/m0/Blink.ino/Blink.ino.ino
@@ -2,76 +2,40 @@
 
 RTCZero rtc;
 
-const byte ledPin = 13;
-
-// Set to random time as we reset the clock every blink cycle
-
-const byte seconds = 0;
-
-const byte minutes = 00;
-
-const byte hours = 17;
-
-const byte day = 17;
-
-const byte month = 11;
-
-const byte year = 15;
-
-// interval in seconds how long led is on/off
-const byte INTERVAL = 1;
-
-boolean isOn = false;
-
+const byte ledPin = 6;
 
 void setup() {
-  while (!Serial); // wait for Serial to be initialized
-  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
   rtc.begin();
-  Serial.println("Starting...");
-  setRTCAlarm();
-  //rtc.standbyMode();
+  rtc.setTime(00, 00, 00);
+  rtc.setDate(00, 00, 00);
+
+  //set alarm timer to 5 seconds later and enable it
+  rtc.setAlarmTime(00, 00, 05);
+  rtc.enableAlarm(rtc.MATCH_HHMMSS);
+
+  rtc.attachInterrupt(ledSwitch);
 }
 
-void loop() {
+void loop()
+{
+  rtc.standbyMode();    // Sleep until next alarm match
 }
 
-
-void blinkLED() {
-   Serial.println("Blinking LED");
-   if(!isOn)
-   {
-   digitalWrite(ledPin, HIGH);
-   } else {
-   digitalWrite(ledPin, LOW);
-   }
-   isOn = !isOn;
-}
-
-// Set rtc and attach interrupt, since we are resetting the clock too
-// the alarm will trigger every INTERVAL seconds
-void setRTCAlarm() {
-  rtc.setTime(hours, minutes, seconds);
-  rtc.setDate(day, month, year);
-  rtc.setAlarmSeconds(INTERVAL);
-  rtc.enableAlarm(RTCZero::MATCH_SS);
-  rtc.attachInterrupt(blinkInInterval);
-  Serial.println("set rtc.");
-}
-
-void blinkInInterval() {
-  blinkLED();
-  setRTCAlarm();
-}
-
-void printTime() {
-  Serial.print(rtc.getHours());
-  Serial.print(":");
-  Serial.print(rtc.getMinutes());
-  Serial.print(":");
-  Serial.print(rtc.getSeconds());
-  Serial.println("");
+//reset internal clock and changes state of LED
+void ledSwitch()
+{
+  if(digitalRead(ledPin) == HIGH)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  else
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  // resets internal clock back to the date "00:00:00" 
+  // (so that the alarm kicks in again in 5 seconds)
+  rtc.setTime(00, 00, 00);
 }
